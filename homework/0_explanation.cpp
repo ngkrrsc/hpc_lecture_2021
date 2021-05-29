dcb76d87df70138532c6b39f4a12e9cc56ac5427
ngkrrsc <82810878+ngkrrsc@users.noreply.github.com>
2021-05-29 19:55:24 +0900

    Update and rename 1_blocking.cpp to 0_explanation.cpp

--- homework/0_explanation.cpp
+++ homework/0_explanation.cpp
@@ -5,65 +5,6 @@
 #include <chrono>
 using namespace std;
 
-#include <mpi.h>
-#include <cstdio>
-#include <cmath>
-#include <vector>
-#include <chrono>
-using namespace std;
-
-#include <cstdlib>
-#include <immintrin.h>
-/*typedef vector<vector<float>> matrix;*/
-
-void matmult(vector &A, vector &B, vector &C, int N) {
-  const int m = N, n = N, k = N;
-  const int kc = 512;
-  const int nc = 64;
-  const int mc = 256;
-  const int nr = 64;
-  const int mr = 32;
-#pragma omp parallel for collapse(2)
-  for (int jc=0; jc<n; jc+=nc) {
-    for (int pc=0; pc<k; pc+=kc) {
-      float Bc[kc*nc];
-      for (int p=0; p<kc; p++) {
-        for (int j=0; j<nc; j++) {
-          Bc[p*nc+j] = B[p+pc][j+jc];
-        }
-      }
-      for (int ic=0; ic<m; ic+=mc) {
-	float Ac[mc*kc],Cc[mc*nc];
-        for (int i=0; i<mc; i++) {
-          for (int p=0; p<kc; p++) {
-            Ac[i*kc+p] = A[i+ic][p+pc];
-          }
-          for (int j=0; j<nc; j++) {
-            Cc[i*nc+j] = 0;
-          }
-        }
-        for (int jr=0; jr<nc; jr+=nr) {
-          for (int ir=0; ir<mc; ir+=mr) {
-            for (int kr=0; kr<kc; kr++) {
-              for (int i=ir; i<ir+mr; i++) {
-                for (int j=jr; j<jr+nr; j++) { 
-                  Cc[i*nc+j] += Ac[i*kc+kr] * Bc[kr*nc+j];
-                }
-              }
-            }
-          }
-        }
-        for (int i=0; i<mc; i++) {
-          for (int j=0; j<nc; j++) {
-            C[i+ic][j+jc] += Cc[i*nc+j];
-          }
-        }
-      }
-    }
-  }
-}
-
-
 int main(int argc, char** argv) {
   /*size:プロセス数, rank:プロセス番号*/
   int size, rank;
