c2f7e39c6071986e8175f48ac5ba28b83b6859fb
ngkrrsc <82810878+ngkrrsc@users.noreply.github.com>
2021-05-31 00:02:04 +0900

    Update 2_simd.cpp

--- 16_final_report/2_simd.cpp
+++ 16_final_report/2_simd.cpp
@@ -17,8 +17,8 @@ void matmult(vector<float> &A, vector<float> &B, vector<float> &C, int N, int M,
   const int nr = 64;
   const int mr = 32;
 #pragma omp parallel for collapse(2)
-  for (int pc=0; pc<k; pc+=kc) {
-    for (int jc=0; jc<n; jc+=nc) {
+  for (int jc=0; jc<n; jc+=nc) {
+    for (int pc=0; pc<k; pc+=kc) {
       float Bc[kc*nc];
       for (int p=0; p<kc; p++) {
         for (int j=0; j<nc; j++) {
@@ -37,8 +37,8 @@ void matmult(vector<float> &A, vector<float> &B, vector<float> &C, int N, int M,
         }
         for (int jr=0; jr<nc; jr+=nr) {
           for (int ir=0; ir<mc; ir+=mr) {
-            for (int i=ir; i<ir+mr; i++) {
-              for (int kr=0; kr<kc; kr++) {
+            for (int kr=0; kr<kc; kr++) {
+              for (int i=ir; i<ir+mr; i++) {
 		__m256 Avec = _mm256_broadcast_ss(Ac+i*kc+kr);
                 for (int j=jr; j<jr+nr; j+=8) {
                   __m256 Bvec = _mm256_load_ps(&Bc[kr*nc+j]);
